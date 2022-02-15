4e401091c6cb9abcd341d6fafd74c2d1dd0d004a
Dewa Mahendra <31196162+mahendraputra21@users.noreply.github.com>
2022-02-15 18:27:44 +0800

    Create Startup.cs

---
+++ SBSender/Startup.cs
@@ -0,0 +1,47 @@
+using SBSender.Data;
+using SBSender.Services;
+
+namespace SBSender
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; set; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+            services.AddSingleton<WeatherForecast>();
+            services.AddTransient<IQueueService, QueueService>();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoint =>
+            {
+                endpoint.MapBlazorHub();
+                endpoint.MapFallbackToPage("/_Host");
+            });
+        }
+    }
+}
