43cec2f4a6d85a972d00dc1ec54cb23ccf218e93
Dewa Mahendra <31196162+mahendraputra21@users.noreply.github.com>
2022-02-15 18:53:16 +0800

    Update Constant.cs

--- SBReceiver/Constants/Constant.cs
+++ SBReceiver/Constants/Constant.cs
@@ -4,7 +4,7 @@ namespace SBReceiver.Constants
     {
         public static class SBReceiverSettings
         {
-            public const string WebApiBaseUrl = "https://localhost:44383/";
+            public const string WebApiBaseUrl = "https://localhost:xxxxx/";
             public const string ConnectionString = "Endpoint=sb://timcoservicebus1.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=<YourKey>";
             public const string QueueName = "personqueue";
             public const int numOfMessages = 3;
